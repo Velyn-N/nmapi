3ccc28b6f959a13e67c2cc94c85d34aded464f1e
Velyn-N <nils01.gaming@gmail.com>
2022-03-11 16:22:47 +0100

    Added CorsFilter.java to allow CORS from all sources

---
+++ src/main/java/de/nmadev/nmapi/rest/CorsFilter.java
@@ -0,0 +1,32 @@
+package de.nmadev.nmapi.rest;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+/**
+ * Since NMApi and NMApp can be deployed separately, we allow CORS (Cross-Origin Resource Sharing) for every host ("*"). <br>
+ * This Filter is non-restrictive, since the API secures itself and is supposed to be accessed from everywhere. <br>
+ * <br>
+ * See also: https://www.baeldung.com/cors-in-jax-rs
+ */
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
